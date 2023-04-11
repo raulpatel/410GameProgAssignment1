8dc023c720ea12a50535ef311c4c0fc6a9d44f5a
Raul Patel <raulp@uoregon.edu>
2023-04-11 01:25:30 -0700

    Double Jump functionality working.

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -6,31 +6,44 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    public float speed = 10;
+    public float jump = 300;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    public GameObject loseTextObject;
 
     private Rigidbody rb;
     private int count;
+    private bool jumpKeyDown;
+    private bool canDoubleJump;
+    private bool grounded;
     private float movementX;
     private float movementY;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
         count = 0;
-
+        canDoubleJump = false;
+        grounded = true;
+        
         SetCountText();
+
         winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+        jumpKeyDown = Input.GetKeyDown(KeyCode.Space);
     }
 
     void OnMove(InputValue movementValue) 
     {
+        jumpKeyDown = Input.GetKeyDown(KeyCode.Space);
         Vector2 movementVector = movementValue.Get<Vector2>();
         
         movementX = movementVector.x;
         movementY = movementVector.y;
+        Jump(jumpKeyDown);
     }
 
     void SetCountText()
@@ -42,11 +55,46 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    void Jump(bool jumpKeyDown) {
+        // double jump logic from https://answers.unity.com/questions/643228/double-jump-2.html
+        if (jumpKeyDown) 
+        {
+            Debug.Log("Jump pressed.");
+            if (grounded) 
+            {
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);//, ForceMode.VelocityChange);
+                rb.AddForce(new Vector3(0, jump, 0));
+                canDoubleJump = true;
+            } 
+            else if (canDoubleJump) 
+            {
+                canDoubleJump = false;
+                rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);//, ForceMode.VelocityChange);
+                rb.AddForce(new Vector3(0, jump, 0));
+            }
+        }
+    }
+
     void FixedUpdate()
     {
+        if (rb.position.y < 0) 
+        {
+            loseTextObject.SetActive(true);
+        }
+        else if (rb.position.y > 0.5)
+        {
+            grounded = false;
+        }
+        else 
+        {
+            grounded = true;
+        }
+        jumpKeyDown = Input.GetKeyDown(KeyCode.Space);
+        Jump(jumpKeyDown);
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         
         rb.AddForce(movement * speed);
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -63,6 +111,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        jumpKeyDown = Input.GetKeyDown(KeyCode.Space);
+        Jump(jumpKeyDown);
     }
 }
